c9a43a1e929bdb7995fbd32b41d74be7d7b932de
akademik <akademuk24@gmail.com>
2025-09-30 17:32:36 +0300

    f

--- script/main.js
+++ script/main.js
@@ -468,3 +468,68 @@ if (addressDisableCheckbox) {
 		}
 	});
 }
+
+
+const FULL_DASH_ARRAY = 283;
+const RESET_DASH_ARRAY = `-57 ${FULL_DASH_ARRAY}`;
+
+// DOM elements
+let timer = document.querySelector("#base-timer-path-remaining");
+let timeLabel = document.getElementById("base-timer-label");
+
+// Time related vars
+const TIME_LIMIT = 120; // in seconds
+let timePassed = -1;
+let timeLeft = TIME_LIMIT;
+let timerInterval = null;
+
+// Условие, проверяющее, нужно ли запускать таймер
+const shouldRunTimer = timer !== null && timeLabel !== null;
+
+if (shouldRunTimer) {
+	window.addEventListener("load", () => {
+		timeLabel.innerHTML = formatTime(TIME_LIMIT);
+		start(); // Auto-start the timer when the page loads
+	});
+
+	function start() {
+		startTimer();
+	}
+
+	function startTimer() {
+		timerInterval = setInterval(() => {
+			timePassed = timePassed += 1;
+			timeLeft = TIME_LIMIT - timePassed;
+			timeLabel.innerHTML = formatTime(timeLeft);
+			setCircleDasharray();
+
+			if (timeLeft === 0) {
+				clearInterval(timerInterval); // Stop the timer when it reaches 0
+			}
+		}, 1000);
+	}
+
+	//---------------------------------------------
+	// HELPER METHODS
+	//---------------------------------------------
+	function formatTime(time) {
+		const minutes = Math.floor(time / 60);
+		let seconds = time % 60;
+
+		if (seconds < 10) {
+			seconds = `0${seconds}`;
+		}
+
+		return `${minutes}:${seconds}`;
+	}
+
+	function calculateTimeFraction() {
+		const rawTimeFraction = timeLeft / TIME_LIMIT;
+		return rawTimeFraction - (1 / TIME_LIMIT) * (1 - rawTimeFraction);
+	}
+
+	function setCircleDasharray() {
+		const circleDasharray = `${(calculateTimeFraction() * FULL_DASH_ARRAY).toFixed(0)} 283`;
+		timer.setAttribute("stroke-dasharray", circleDasharray);
+	}
+}
